e6e443249e98a246f882ce9008847b69e3b35e74
Charlie Malone <charliemalonesf@icloud.com>
2026-03-06 15:49:27 -0800

    Update start_gunicorn.sh

--- scripts/start_gunicorn.sh
+++ scripts/start_gunicorn.sh
@@ -1,4 +1,2 @@
 #!/usr/bin/env bash
-PORT=${PORT:-8000}
-
-gunicorn -w 4 -b 0.0.0.0:$PORT router:app --timeout 600 --access-logfile -
+gunicorn -w 4 -b 0.0.0.0:8000 router:app --timeout 600 --access-logfile -
