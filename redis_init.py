7cca9c9175d12670b1380db5102335b2050efb59
Charlie Malone <charliemalonesf@icloud.com>
2026-03-06 16:57:50 -0800

    Initialize Redis client in redis_init.py

--- redis_init.py
+++ redis_init.py
@@ -1,4 +1,3 @@
-import os
 import redis
 
 REDIS_HOST = "redis.svc.tools.eqiad1.wikimedia.cloud"
@@ -6,5 +5,11 @@ REDIS_PORT = 6379
 REDIS_DB = 9
 
 redis_url = f"redis://{REDIS_HOST}:{REDIS_PORT}/{REDIS_DB}"
+
+rediscl = redis.Redis(
+    host=REDIS_HOST,
+    port=REDIS_PORT,
+    db=REDIS_DB
+)
 
 REDIS_KEY_PREFIX = "mw-toolforge-buckbot"
