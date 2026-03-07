4ccb580e7d996a6e67cc8d0047f7c63e84163d28
Charlie Malone <charliemalonesf@icloud.com>
2026-03-06 17:14:55 -0800

    Add echo statement before starting Celery worker

--- scripts/start_celery.sh
+++ scripts/start_celery.sh
@@ -6,6 +6,8 @@ rm -f user-config.py
 cp user-config.tmpl user-config.py
 chmod 600 user-config.py
 
+echo "Starting Celery worker..."
+
 celery -A app:celery worker \
   --loglevel=INFO \
   --concurrency=2 \
