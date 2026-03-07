a987846f6a8c903e2929ef8b37015b2920dc6e4e
Charlie Malone <charliemalonesf@icloud.com>
2026-03-06 16:45:58 -0800

    Update start_celery.sh to refresh user-config.py
    Remove old user-config.py and copy from template.

--- scripts/start_celery.sh
+++ scripts/start_celery.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
-cat user-config.tmpl > user-config.py
+rm -f user-config.py
+cp user-config.tmpl user-config.py
 chmod 600 user-config.py
-chown $(id -u):$(id -g) user-config.py
 
 celery -A app:celery worker \
   --loglevel=INFO \
