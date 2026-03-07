58be9b77cddca6838cec05577634112f02f039f5
Charlie Malone <charliemalonesf@icloud.com>
2026-03-06 17:22:37 -0800

    Set environment variables for Celery
    Add environment variables for Celery configuration

--- scripts/start_celery.sh
+++ scripts/start_celery.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 set -e
+
 export C_FORCE_ROOT=true
 export CELERY_BROKER_URL="redis://redis.svc.tools.eqiad1.wikimedia.cloud:6379/9"
 export CELERY_RESULT_BACKEND="redis://redis.svc.tools.eqiad1.wikimedia.cloud:6379/9"
+
 rm -f user-config.py
 cp user-config.tmpl user-config.py
 chmod 600 user-config.py
