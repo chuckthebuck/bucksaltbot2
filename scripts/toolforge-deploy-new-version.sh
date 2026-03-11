f23df4a763b257a5561457f873e92af51f772ba4
Charlie Malone <charliemalonesf@icloud.com>
2026-03-11 10:15:13 -0700

    Update deployment script to retain specific jobs
    Removed deletion of buckbot-celery and buckbot-ping jobs.

--- scripts/toolforge-deploy-new-version.sh
+++ scripts/toolforge-deploy-new-version.sh
@@ -3,8 +3,6 @@ set -ex
 cd 
 git pull
 toolforge build start https://github.com/chuckthebuck/bucksaltbot2
-toolforge jobs delete buckbot-celery
-toolforge jobs delete buckbot-ping
 toolforge webservice restart
 toolforge jobs load jobs.yaml
 toolforge jobs list
