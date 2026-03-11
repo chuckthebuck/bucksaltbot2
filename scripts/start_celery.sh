c8dac16057f37abbfcf9f9af74e2fe503ede2674
Charlie Malone <charliemalonesf@icloud.com>
2026-03-11 13:41:57 -0700

    Update start_celery.sh

--- scripts/start_celery.sh
+++ scripts/start_celery.sh
@@ -3,8 +3,6 @@ set -e
 
 export C_FORCE_ROOT=true
 
-export CELERY_BROKER_URL="redis://redis.svc.tools.eqiad1.wikimedia.cloud:6379/9"
-export CELERY_RESULT_BACKEND="redis://redis.svc.tools.eqiad1.wikimedia.cloud:6379/9"
 
 rm -f user-config.py
 cp user-config.tmpl user-config.py
