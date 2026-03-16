23741c0ca6c0574cdd51e85810229029b48489fb
Charlie Malone <charliemalonesf@icloud.com>
2026-03-16 15:53:44 -0700

    Update script.ts

--- client-src/script.ts
+++ client-src/script.ts
@@ -1,12 +1,6 @@
+import { createApp, ref, watch } from 'vue'
 import * as Codex from '@wikimedia/codex'
 import '@wikimedia/codex/dist/codex.style.css'
 
-/*
-Expose Codex globally so existing code like
-Codex.lookup(...) works.
-*/
+;(window as any).Vue = { createApp, ref, watch }
 ;(window as any).Codex = Codex
-
-window.addEventListener('load', () => {
-    console.log('Buckbot UI loaded')
-})
