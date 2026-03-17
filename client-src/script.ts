6cd93e755c310119b9dac01d42ed92c40fd5fec9
Charlie Malone <charliemalonesf@icloud.com>
2026-03-16 20:31:41 -0700

    Update script.ts

--- client-src/script.ts
+++ client-src/script.ts
@@ -1,6 +1,6 @@
-import { createApp, ref, watch } from 'vue'
-import * as Codex from '@wikimedia/codex'
-import '@wikimedia/codex/dist/codex.style.css'
+import * as Vue from "vue";
+import * as Codex from "@wikimedia/codex";
+import "@wikimedia/codex/dist/codex.style.css";
 
-;(window as any).vue = { createApp, ref, watch }
-;(window as any).Codex = Codex
+(window as any).Vue = Vue;
+(window as any).Codex = Codex;
