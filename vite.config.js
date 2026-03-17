ca9e829aa73b1d9bbce6b92628402c6f18752434
Charlie Malone <charliemalonesf@icloud.com>
2026-03-17 12:18:06 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,34 +1,22 @@
 import path from "node:path";
 import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
   root: path.join(__dirname, "./client-src/"),
   base: "/assets/",
-
+  plugins: [vue()],
   resolve: {
-    dedupe: ["vue"], 
+    dedupe: ["vue"],
   },
-
-  optimizeDeps: {
-    include: ["vue", "@wikimedia/codex"], 
-  },
-
   build: {
     outDir: path.join(__dirname, "./assets_compiled/"),
     manifest: "manifest.json",
     assetsDir: "bundled",
     emptyOutDir: true,
     copyPublicDir: false,
-
     rollupOptions: {
-      input: [
-        "client-src/script.ts",
-      ],
-
-      output: {
-        format: "iife", 
-        entryFileNames: "bundled/script-[hash].js",
-      },
+      input: "script.ts",
     },
   },
 });
