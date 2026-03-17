554eface5dd2a43c43d1f844fbbf246ac47ce0f2
Charlie Malone <charliemalonesf@icloud.com>
2026-03-17 13:39:19 -0700

    Update script.ts

--- client-src/script.ts
+++ client-src/script.ts
@@ -3,6 +3,7 @@ console.log("🔥 ENTRY FILE RUNNING 🔥");
 import { createApp } from "vue";
 import App from "./App.vue";
 import BatchApp from "./BatchApp.vue";
+import "@wikimedia/codex/dist/codex.style.css";
 
 if (document.getElementById("batch-props")) {
   createApp(BatchApp).mount("#app");
