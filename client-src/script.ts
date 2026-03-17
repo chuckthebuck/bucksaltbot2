29c835f07732fcbcb47113353bff95a0e04e9f41
Charlie Malone <charliemalonesf@icloud.com>
2026-03-16 20:34:53 -0700

    Update script.ts

--- client-src/script.ts
+++ client-src/script.ts
@@ -1,6 +1,7 @@
 import * as Vue from "vue";
 import * as Codex from "@wikimedia/codex";
 import "@wikimedia/codex/dist/codex.style.css";
+import "./styles.less"
 
 (window as any).Vue = Vue;
 (window as any).Codex = Codex;
