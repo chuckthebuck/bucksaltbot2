3a5d79bd98d387dc9ce378bc22f597088367b02b
Charlie Malone <charliemalonesf@icloud.com>
2026-03-17 18:59:46 -0700

    Update script.ts

--- client-src/script.ts
+++ client-src/script.ts
@@ -1,9 +1,9 @@
 console.log("🔥 ENTRY FILE RUNNING 🔥");
-
+import "@wikimedia/codex/dist/codex.style.css";
+import "./styles.less"; // 👈 you already have this file, USE IT
 import { createApp } from "vue";
 import App from "./App.vue";
 import BatchApp from "./BatchApp.vue";
-import "@wikimedia/codex/dist/codex.style.css";
 
 if (document.getElementById("batch-props")) {
   createApp(BatchApp).mount("#app");
